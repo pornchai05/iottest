90850ab9461a4c6e07763e0354915c37ba692422
pornchai05 <113157797+pornchai05@users.noreply.github.com>
2023-02-26 16:38:24 +0700

    first time

---
+++ iotlinebotpornchaip.py
@@ -0,0 +1,95 @@
+from flask import Flask, request
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import MessageEvent, TextMessage,TextSendMessage
+
+import paho.mqtt.client as mqttClient
+import time
+
+temp = ""
+humi = ""
+
+def on_message(client, userdata, msg):
+    global temp,humi
+    print(msg.topic+" "+str(msg.payload))
+    text_t_h = msg.payload.decode('UTF-8')
+    t_and_h = text_t_h.split(',')
+    temp = t_and_h[0]
+    humi = t_and_h[1]
+
+channel_secret = "bf90a6df959a27c8304e106c5353c8cf"
+channel_access_token = "LpGE/5XlyYdH9pksAL/Be1vrrvgOs21EHnz6H2FtPXViW6BnOVBH98VTMSt5i8lA0hgYM4mnFeHqbXQqxQ+dQTjyS1NdO6p8az/VumExv3XxDIroj2KZxQClpFbTqq629isBaGk5Phq55aVTRLvCWwdB04t89/1O/w1cDnyilFU="
+
+broker_address= "mqtt.netpie.io"
+port = 1883
+
+client = mqttClient.Client("ccce69c0-50ed-4e42-b569-7e3fc1c638a2") # Client ID
+user = "dX13vhD1Psr8RXzJ3jJMbnzBWgbxkbho" # Token
+password = "rYneFgMFR_Yg3y*Vz*BJ)eR-JEg3vV!M" # Secret
+
+client.username_pw_set(user, password=password)    
+client.on_message = on_message
+
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET","POST"])
+def home():
+    try:
+        signature = request.headers["X-Line-Signature"]
+        body = request.get_data(as_text=True)
+        handler.handle(body, signature)
+    except:
+        pass
+    
+    return "Hello Line Chatbot"
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_text_message(event):
+    global temp,humi
+    text = event.message.text
+    print(text)
+
+    try:
+        client.connect(broker_address, port=port)        
+    except:
+        print("Connection failed")
+    
+    if (text=="เปิดไฟ"):
+        client.publish("@msg/led","ledon")
+        text_out = "เปิดไฟเรียบร้อยแล้ว"
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=text_out))
+
+    if (text=="ปิดไฟ"):
+        client.publish("@msg/led","ledoff")
+        text_out = "ปิดไฟเรียบร้อยแล้ว"
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=text_out))
+
+    if (text=="สีแดง"):
+        client.publish("@msg/color","red")
+        text_out = "เปิดไฟสีแดงเรียบร้อยแล้ว"
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=text_out))
+
+    if (text=="สีเขียว"):
+        client.publish("@msg/color","green")
+        text_out = "เปิดไฟสีเขียวเรียบร้อยแล้ว"
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=text_out))
+
+    if (text=="สีน้ำเงิน"):
+        client.publish("@msg/color","blue")
+        text_out = "เปิดไฟสีน้ำเงินเรียบร้อยแล้ว"
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=text_out))
+
+    if (text=="อุณหภูมิและความชื้น"):
+        client.subscribe("@msg/t_h")
+        client.loop_start()
+        time.sleep(1.5) 
+        client.loop_stop()
+        if len(temp) > 0 and len(humi) > 0:
+            text_out = "อุณหภูมิ " + temp + " ความชื้น " + humi
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text=text_out))
+                     
+if __name__ == "__main__":          
+    app.run()
+
